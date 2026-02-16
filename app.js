93d0c828c2c12b7f9736e9ab884f658bc51be368
alat414 <gustavo.alatriste@g.austincc.edu>
2026-02-15 19:10:58 -0600

    adding jwt verify function to ensure token is valid; adding return statement, a 403 error, else, the function continues following the middleware

--- app.js
+++ app.js
@@ -29,7 +29,12 @@ function authenticateToken(req, res, nex)
     
     if (token == null) return res.sendStatus(401)
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user))
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => 
+    {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
 
 }
 app.listen(port, () => 
