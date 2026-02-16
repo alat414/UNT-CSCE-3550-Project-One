bf98836f470089c164f8c2e02a819ec919aa18f2
alat414 <gustavo.alatriste@g.austincc.edu>
2026-02-15 23:13:05 -0600

    Reusing commands from app.js file, including const express, app, and jwt

--- keyExpiry.js
+++ keyExpiry.js
@@ -1,4 +1,23 @@
 // Authenticate User
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+const port = 8080;
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+app.post('/login', (req, res) => 
+{
+    const username = req.body.username
+    const user = { name: username }
+
+    const accessToken = generateToken(user)
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    res.json({ accessToken: accessToken});
+
+});
 
 function generateToken(user)
 {
