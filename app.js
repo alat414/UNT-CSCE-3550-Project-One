933f3dc40889fcbd44b4f33d80fc2040e6fbf81f
alat414 <gustavo.alatriste@g.austincc.edu>
2026-02-16 23:44:22 -0600

    adding expiration to the accesstoken; fixing authenticatetoken with next function call; removing listening call

--- app.js
+++ app.js
@@ -30,12 +30,12 @@ app.post('/login', (req, res) =>
     const username = req.body.username
     const user = { name: username }
 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s'})
     res.json({ accessToken: accessToken});
 
 });
 
-function authenticateToken(req, res, nex)
+function authenticateToken(req, res, next)
 {
     const authHeader = req.headers['authorization'] 
     const token = authHeader && authHeader.split(' ')[1]
@@ -46,7 +46,7 @@ function authenticateToken(req, res, nex)
     {
         if (err) return res.sendStatus(403)
         req.user = user
-        nex() 
+        next() 
     })
 
 }
@@ -57,7 +57,4 @@ module.exports =
     authenticateToken,
     posts
 }
-app.listen(port, () => 
-{
-    console.log(`Example app listening at http://localhost:${8080}`);
-});
+
