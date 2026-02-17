76f1cfaf1e4024ccab3b279e39ee93512f33d91a
alat414 <gustavo.alatriste@g.austincc.edu>
2026-02-17 00:05:50 -0600

    copy of working servers with expiry tokens

--- keyExpiry.js
+++ keyExpiry.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8081;
 const jwt = require('jsonwebtoken')
 
 const { authenticateToken, posts } = require('./app.js')
@@ -60,6 +60,6 @@ function generateToken(user)
 
 app.listen(port, () => 
 {
-    console.log(`Example app listening at http://localhost:${8080}`);
+    console.log(`Example app listening at http://localhost:${8081}`);
     console.log(`Using authenticateToken from app.js`);
 });
