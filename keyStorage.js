cd1c365c985fd1c86e447673e427eb28cadac125
alat414 <gustavo.alatriste@g.austincc.edu>
2026-02-17 09:35:50 -0600

    adding string to generate key function including hexadecimal data and data in certain format

--- keyStorage.js
+++ keyStorage.js
@@ -8,4 +8,9 @@ class keyStorage
         this.validKeyID = null;
     }
 
+    generateNewKey(expiresInMinutes = 1)
+    {
+        const keyID = `key-${new Date().toIS0String().split('T')[0]}- ${crypto.randomBytes(4).toString('hex')}`;
+    }
+
 }
