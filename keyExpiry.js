bfe6b8cedd2bea11c51d473298eac27f00822730
alat414 <gustavo.alatriste@g.austincc.edu>
2026-02-19 11:05:31 -0600

    editing algorithm typo

--- keyExpiry.js
+++ keyExpiry.js
@@ -86,7 +86,7 @@ app.post('/login', (req, res) =>
             header: 
             {
                 kid: activeKeyID,
-                alg: 'H256'
+                alg: 'HS256'
             }
         }
     );
