5d648dbac21410e7e43d8da7cadaff5f7e2d64b4
alat414 <gustavo.alatriste@g.austincc.edu>
2026-02-18 20:14:01 -0600

    adding getkey call from keystorage; adding error message when no signing key exists or if key is expired

--- app.js
+++ app.js
@@ -37,11 +37,30 @@ app.post('/login', (req, res) =>
 
 function authenticateToken(req, res, next)
 {
-    const authHeader = req.headers['authorization'] 
-    const token = authHeader && authHeader.split(' ')[1]
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
     
-    if (token == null) return res.sendStatus(401)
+    if (token == null) return res.sendStatus(401);
 
+    const decodedHeader = jwt.decode(token, { complete: true });
+
+    if (!decodedHeader || !decodedHeader.header || !decodedHeader.header.kid)
+    {
+        return res.status(401).json({ error: 'No key ID in token' });
+    }
+
+    const keyID = decodedHeader.header.kid;
+
+    const signingKey = keyStorage.getKey(keyID);
+
+    if(!signingKey)
+    {
+        return res.status(401).json
+        ({
+            error: 'Key invalid',
+            message: 'Token was signed with invalid key, retry.'
+        });
+    }
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => 
     {
         if (err) return res.sendStatus(403)
