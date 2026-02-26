b1ac4a4ebb155007076d4b1ec5bf97473877aab3
alat414 <gustavo.alatriste@g.austincc.edu>
2026-02-25 18:38:20 -0600

    adding correct names and titles

--- testing/fixture_tests/test-users.js
+++ testing/fixture_tests/test-users.js
@@ -1,5 +1,20 @@
 module.exports=
 {
     VALID_USERS: [Nanna, Raggi],
-    INVALID_USERS: [null, undefined]
+    INVALID_USERS: ['hacker', null, undefined],
+    TEST_POSTS: 
+    [{
+        username: 'Nanna',
+        title: 'lead_singer',
+
+        username: 'Raggi',
+        title: 'co-singer',
+        
+        username: 'Nanna',
+        title: 'rhythm guitarist',
+        
+        username: 'Raggi',
+        title: 'lead guitarist',
+    }
+    ]
 }
