920aba750ff000ae80bc6c45b48940cfdc9789ad
alat414 <gustavo.alatriste@g.austincc.edu>
2026-02-27 20:08:54 -0600

    adding comment header block

--- testing/setup/testsEnvironment.js
+++ testing/setup/testsEnvironment.js
@@ -1,1 +1,7 @@
-ECHO is on.
+/* *************************************************
+*  Name: Gustavo Alatriste
+*  Assignment: Project One JWKS server
+*  Purpose: A file  of a properly
+*           constructed and commented 
+*           testsEnvironment.cpp
+************************************************* */
