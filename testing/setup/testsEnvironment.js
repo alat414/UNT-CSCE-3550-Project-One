4c86654d9d29d60969a60be17619f5fe47b35f8a
alat414 <gustavo.alatriste@g.austincc.edu>
2026-02-27 20:24:39 -0600

    setting environment variables to access,refresh tokens, and port running tests, used different port for testing

--- testing/setup/testsEnvironment.js
+++ testing/setup/testsEnvironment.js
@@ -8,3 +8,11 @@
 
 require('dotenv').config({path: '.env.test'});
 
+process.env.ACCESS_TOKEN_SECRET = 'test-access-secret';
+process.env.REFRESH_TOKEN_SECRET = 'test-refresh-secret';
+process.env.PORT = 8082;
+
+const request = require('supertest');
+const app = require('../../keyExpiry');
+
+module.exports = {request, app};
