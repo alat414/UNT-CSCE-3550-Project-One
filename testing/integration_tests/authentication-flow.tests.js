8cf7a512f83e9178f21363b7ee14c031cb00a4b1
alat414 <gustavo.alatriste@g.austincc.edu>
2026-02-28 19:35:27 -0600

    importing modules from local dir, both environments and key testing funcs

--- testing/integration_tests/authentication-flow.tests.js
+++ testing/integration_tests/authentication-flow.tests.js
@@ -7,3 +7,14 @@
 *           error, and key (ID) returns 
 *           authentication-flow.tests.js
 ************************************************* */
+
+
+/* *************************************************
+*  Importing a local module in both 
+*  testing environment and 
+*  key testing functions.
+************************************************* */
+const {request, app } = require('../setup/testsEnvironment');
+const { VALID_USERS, INVALID_USERS, EXPECTED_POSTS } = require('../fixture_tests/test-keys');
+
+
