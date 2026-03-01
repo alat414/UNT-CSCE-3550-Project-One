efe18c0c16adb6a4755ac417254d5ee7bc38da3b
alat414 <gustavo.alatriste@g.austincc.edu>
2026-03-01 14:02:03 -0600

    adding comment header block

--- testing/unit_tests/keyStorage.tests.js
+++ testing/unit_tests/keyStorage.tests.js
@@ -1,1 +1,9 @@
-ECHO is on.
+/* *************************************************
+*  Name: Gustavo Alatriste
+*  Assignment: Project One JWKS server
+*  Purpose: Key Storage testing functions
+*           using group tests and blocks to 
+*           ensure proper POST, GET, token,
+*           error, and key(ID) returns 
+*           keyStorage.tests.js
+************************************************* */
