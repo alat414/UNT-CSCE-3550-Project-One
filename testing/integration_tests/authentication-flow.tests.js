761042c6906af7187e8af376566e50f79be65c0d
alat414 <gustavo.alatriste@g.austincc.edu>
2026-02-28 19:28:24 -0600

    adding comment header block

---
+++ testing/integration_tests/authentication-flow.tests.js
@@ -0,0 +1,9 @@
+/* *************************************************
+*  Name: Gustavo Alatriste
+*  Assignment: Project One JWKS server
+*  Purpose: Authentication testing functions
+*           using group tests and blocks to 
+*           ensure proper POST, GET, token,
+*           error, and key (ID) returns 
+*           authentication-flow.tests.js
+************************************************* */
